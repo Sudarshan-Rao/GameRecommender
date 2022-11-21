7c95ca60074d8143cffb8a6d06a3ac49064213e8
Azam <mazam1488@gmail.com>
2022-11-21 16:02:04 -0500

    add data to Tags table

--- api/scripts/preprocess_tags.py
+++ api/scripts/preprocess_tags.py
@@ -1,4 +1,5 @@
 from fuzzywuzzy import fuzz
+import sqlite3
 
 game_desc = """Diamond is a fast paced puzzle game demanding quick reflexes as you control a ball with perpetual vertical momentum through a wealth of various levels. With over 30 individual puzzle components, the goal of destroying gems is rarely ever as simple as matching your color and hitting them.
 
@@ -10,6 +11,16 @@ In addition to over a hundred levels is the same fully featured editor used to c
 ratio = fuzz.token_set_ratio(game_desc, "quick")
 
 print(ratio)
+
+with open("../resources/preset_keywords.txt") as file:
+    lines = [line.rstrip() for line in file]
+
+db_connection = sqlite3.connect("../resources/preprocess.db")
+db_cursor = db_connection.cursor()
+
+for line in lines:
+    db_cursor.execute("INSERT INTO Tags VALUES (?, ?)", (None, line))
+db_connection.commit()
 
 
 # make a table with matching phrase for tags, each tag wll have itself as a matching phrase, column preprocessed?t/f
